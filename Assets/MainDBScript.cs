d9bcaef9e3e8a1180b0434dc1ced68c353552e79
taylorjames9 <taylorjames9@gmail.com>
2017-08-16 21:20:22 -0700

    now you can add a new comment to the database when you hit enter on the comment you send

--- Assets/MainDBScript.cs
+++ Assets/MainDBScript.cs
@@ -6,26 +6,41 @@ using Firebase.Unity.Editor;
 
 public class MainDBScript : MonoBehaviour
 {
-	DatabaseReference reference; 
-	
-	[SerializeField]
-	private GameObject parentOfAllPrompts;
+    DatabaseReference reference;
 
-	public GameObject promptHolder_prefab;
-	public GameObject emptyMet;
+    [SerializeField]
+    private GameObject parentOfAllPrompts;
 
-	public GameObject InputField;
-	    void Start()
+    public GameObject promptHolder_prefab;
+    public GameObject emptyMet;
+
+    //public GameObject InputField;
+
+	public MainDBScript Instance {get; private set;}
+
+    private DataSnapshot db_prompts;
+    void Start()
+    {
+		Instance = this;
+        Setup();
+    }
+
+    public void SendResponse(GameObject sender)
+    {
+        writeNewResponse(sender.GetComponent<InputField>().text);
+        Debug.Log("Send Response Fired. Response is : "+sender.GetComponent<InputField>().text);
+        RefreshFeed();
+    }
+
+
+    private void RefreshFeed()
     {
         // Set this before calling into the realtime database.
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://projectmetaphor2.firebaseio.com/");
 
         // Get the root reference location of the database.
         reference = FirebaseDatabase.DefaultInstance.RootReference;
-		Debug.Log(reference.ToString());
-
-        /////writeNewUser("taylorjames9", "James Taylor", "taylorjames9@gmail.com");
-        /////writeNewUser("billoby", "Bill Wighton", "bill@hotmail.com");
+        Debug.Log(reference.ToString());
 
         FirebaseDatabase.DefaultInstance
   .GetReference("prompts")
@@ -39,75 +54,153 @@ public class MainDBScript : MonoBehaviour
       else if (task.IsCompleted)
       {
           DataSnapshot allPrompts = task.Result;
-          Debug.Log("Snapshot = " + allPrompts.ChildrenCount.ToString());
-          Debug.Log("printing content of prompt 3" + allPrompts.Child("prompt003").Child("content").GetRawJsonValue());
-		  ///Write Comments to DB
-		  writeNewResponse("tay001", "Jamessss", 1, "like shiny pennies", allPrompts);
+          db_prompts = allPrompts;
 
           int i = 1;
-		  foreach (var prompt in allPrompts.Children)
+          foreach (var prompt in allPrompts.Children)
           {
-				Debug.Log("00"+prompt.Child("promptID").GetRawJsonValue());
-				GameObject newPromptAndContent_Empty = Instantiate(promptHolder_prefab, parentOfAllPrompts.transform, true) as GameObject;
-				newPromptAndContent_Empty.name = "Prompt00"+prompt.Child("promptID").GetRawJsonValue();
-				newPromptAndContent_Empty.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = allPrompts.Child("prompt00"+i.ToString()).Child("content").GetRawJsonValue().ToString();
-				Debug.Log("prompt child count "+prompt.ChildrenCount);
-				int r = 1;
-				foreach(var cmt in prompt.Child("comments").Children){
-					Debug.Log("content for prompt"+cmt.Child("content").GetRawJsonValue());
-					GameObject newResponse = Instantiate(emptyMet, newPromptAndContent_Empty.transform, true) as GameObject;
-					newResponse.name = "response00"+r.ToString();
-					newResponse.transform.GetChild(0).GetComponent<Text>().text = cmt.Child("content").GetRawJsonValue();
-					r++;
-				}
-				//loop through each comment, check to see if its text is empty, delete it if empty
-				foreach(Transform t in newPromptAndContent_Empty.transform){
-					if(t.GetChild(0).GetComponent<Text>() && t.GetChild(0).GetComponent<Text>().text.Equals("")){
-						Destroy(t.gameObject);
-					}
-				}
-				i++;
+              GameObject newPromptAndContent_Empty = Instantiate(promptHolder_prefab, parentOfAllPrompts.transform, true) as GameObject;
+              newPromptAndContent_Empty.name = "Prompt00" + prompt.Child("promptID").GetRawJsonValue();
+              newPromptAndContent_Empty.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = allPrompts.Child("prompt00" + i.ToString()).Child("content").GetRawJsonValue().ToString();
+              int r = 1;
+              foreach (var cmt in prompt.Child("comments").Children)
+              {
+                  GameObject newResponse = Instantiate(emptyMet, newPromptAndContent_Empty.transform, true) as GameObject;
+                  newResponse.name = "response00" + r.ToString();
+                  newResponse.transform.GetChild(0).GetComponent<Text>().text = cmt.Child("content").GetRawJsonValue();
+                  r++;
+              }
+              //loop through each comment, check to see if its text is empty, delete it if empty
+              foreach (Transform t in newPromptAndContent_Empty.transform)
+              {
+                  if (t.GetChild(0).GetComponent<Text>() && t.GetChild(0).GetComponent<Text>().text.Equals(""))
+                  {
+                      Destroy(t.gameObject);
+                  }
+              }
+              i++;
           }
       }
   });
+    }
 
+
+
+
+    #region Setup
+    private void Setup()
+    {
+        // Set this before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://projectmetaphor2.firebaseio.com/");
+
+        // Get the root reference location of the database.
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        Debug.Log(reference.ToString());
+
+        FirebaseDatabase.DefaultInstance
+  .GetReference("prompts")
+  .GetValueAsync().ContinueWith(task =>
+  {
+      if (task.IsFaulted)
+      {
+          // Handle the error...
+          Debug.Log("There was an error retrieving the data...");
+      }
+      else if (task.IsCompleted)
+      {
+          DataSnapshot allPrompts = task.Result;
+          db_prompts = allPrompts;
+          Debug.Log("Snapshot = " + allPrompts.ChildrenCount.ToString());
+          Debug.Log("printing content of prompt 3" + allPrompts.Child("prompt003").Child("content").GetRawJsonValue());
+          ///Write Comments to DB
+          ////writeNewResponse("tay001", "Jamessss", 1, "like shiny pennies", allPrompts);
+
+          int i = 1;
+          foreach (var prompt in allPrompts.Children)
+          {
+              Debug.Log("00" + prompt.Child("promptID").GetRawJsonValue());
+              GameObject newPromptAndContent_Empty = Instantiate(promptHolder_prefab, parentOfAllPrompts.transform, true) as GameObject;
+              newPromptAndContent_Empty.name = "Prompt00" + prompt.Child("promptID").GetRawJsonValue();
+              newPromptAndContent_Empty.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = allPrompts.Child("prompt00" + i.ToString()).Child("content").GetRawJsonValue().ToString();
+              Debug.Log("prompt child count " + prompt.ChildrenCount);
+              int r = 1;
+              foreach (var cmt in prompt.Child("comments").Children)
+              {
+                  Debug.Log("content for prompt" + cmt.Child("content").GetRawJsonValue());
+                  GameObject newResponse = Instantiate(emptyMet, newPromptAndContent_Empty.transform, true) as GameObject;
+                  newResponse.name = "response00" + r.ToString();
+                  newResponse.transform.GetChild(0).GetComponent<Text>().text = cmt.Child("content").GetRawJsonValue();
+                  r++;
+              }
+              //loop through each comment, check to see if its text is empty, delete it if empty
+              foreach (Transform t in newPromptAndContent_Empty.transform)
+              {
+                  if (t.GetChild(0).GetComponent<Text>() && t.GetChild(0).GetComponent<Text>().text.Equals(""))
+                  {
+                      Destroy(t.gameObject);
+                  }
+              }
+              i++;
+          }
+      }
+  });
+    }
+    #endregion
+
+
+    private void writeNewResponse(string content)
+    {
+        Response response = new Response(content);
+        string rep_json = JsonUtility.ToJson(response);
+        int childCount = (int)db_prompts.Child("prompt001").Child("comments").ChildrenCount + 1;
+        reference.Child("prompts").Child("prompt001").Child("comments").Child("comment00" + childCount).SetRawJsonValueAsync(rep_json);
+        string key = reference.Child("prompts").Child("prompt001").Push().Key;
+        Debug.Log("Write Response Fired.");
 
     }
+
+    private void writeNewResponse(string userID, string name, int promptID, string content, DataSnapshot allPrompts)
+    {
+        Response response = new Response(name, promptID, content);
+        string rep_json = JsonUtility.ToJson(response);
+        //int childCount = reference.Child("prompts").Child("prompt00"+promptID).Child("comments").ChildrenCount;
+        int childCount = (int)allPrompts.Child("prompt00" + promptID).Child("comments").ChildrenCount + 1;
+
+        reference.Child("prompts").Child("prompt00" + promptID).Child("comments").Child("comment00" + childCount).SetRawJsonValueAsync(rep_json);
+        string key = reference.Child("prompts").Child("prompt00" + promptID).Push().Key;
+        Debug.Log("Finished adding new comment.. ...");
+    }
+
     private void writeNewUser(string userId, string name, string email)
     {
         User user = new User(name, email);
         string json = JsonUtility.ToJson(user);
 
         //mDatabaseRef.Child("users").Child(userId).SetRawJsonValueAsync(json);
-		reference.Child("users").Child(userId).SetRawJsonValueAsync(json);
-		string key = reference.Child("users").Push().Key;
-		Debug.Log("Finished execution ...");
+        reference.Child("users").Child(userId).SetRawJsonValueAsync(json);
+        string key = reference.Child("users").Push().Key;
+        Debug.Log("Finished execution ...");
     }
-
-	private void writeNewResponse(string userID, string name, int promptID, string content, DataSnapshot allPrompts){
-		Response response = new Response(name, promptID, content);
-		string rep_json = JsonUtility.ToJson(response);
-		//int childCount = reference.Child("prompts").Child("prompt00"+promptID).Child("comments").ChildrenCount;
-		int childCount = (int)allPrompts.Child("prompt00"+promptID).Child("comments").ChildrenCount+1;
-
-		reference.Child("prompts").Child("prompt00"+promptID).Child("comments").Child("comment00"+childCount).SetRawJsonValueAsync(rep_json);
-		string key = reference.Child("prompts").Child("prompt00"+promptID).Push().Key;
-		Debug.Log("Finished adding new comment.. ...");
-	}
-
 }
 
-public class Response{
-	public string userID = "null for now";
-	public string name;
 
-	public int promptID;
-	public string content;
-	public Response(string name, int promptID, string content){
-		this.name = name;
-		this.promptID = promptID;
-		this.content = content;
-	}
+public class Response
+{
+    public string userID = "null for now";
+    public string name;
+
+    public int promptID;
+    public string content;
+    public Response(string content)
+    {
+        this.content = content;
+    }
+    public Response(string name, int promptID, string content)
+    {
+        this.name = name;
+        this.promptID = promptID;
+        this.content = content;
+    }
 }
 
 
